f56b8e9a480fbe78e80e9b551b46fae82c6d335c
dreamIIx <shiba.zheka65@gmail.com>
2021-03-05 19:42:12 +0700

    upd 3

--- Source/main1.c
+++ Source/main1.c
@@ -29,31 +29,33 @@ int main()
     pairShort points[__N * __N];
     size_t szPoints = 0ull;
 
-    for (size_t i = 0ull; i < x; ++i)
+    for(size_t i = 0ull; i < x; ++i)
     {
-#ifdef _INC_LIMITS
-        inf[i].max = _I32_MIN;
-#elif defined _GCC_LIMITS_H_
-        inf[i].max = INT_MAX;
-#else
-#error "Cannot provide INTMAX"
-#endif
         inf[i].average = 0;
     }
 
     for (size_t i = 0ull; i < y; ++i)
     {
-#ifdef _INC_LIMITS
-        inf[i].min = _I32_MAX;
-#elif defined _GCC_LIMITS_H_
-        inf[i].min = INT_MIN;
-#else
-#error "Cannot provide INTMAX"
-#endif
         for (size_t j = 0ull; j < x; ++j)
         {
             scanf("%i", &mtx[i][j]);
             inf[j].average += mtx[i][j];
+        }
+    }
+
+    for (size_t i = 0ull; i < y; ++i)
+    {
+        inf[i].min = mtx[i][0];
+    }
+    for(size_t i = 0ull; i < x; ++i)
+    {
+        inf[i].max = mtx[0][i];
+    }
+
+    for (size_t i = 0ull; i < y; ++i)
+    {
+        for (size_t j = 0ull; j < x; ++j)
+        {
             if (mtx[i][j] < inf[i].min) inf[i].min = mtx[i][j];
             if (mtx[i][j] > inf[j].max) inf[j].max = mtx[i][j];
         }
