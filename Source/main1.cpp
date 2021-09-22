be87399fc1028e2b890fa4407d292e686bc5982f
dreamIIx <shiba.zheka65@gmail.com>
2021-09-22 23:32:35 +0700

    major fix

--- Source/main1.cpp
+++ Source/main1.cpp
@@ -44,10 +44,12 @@
 #define ER_IFN(x, beforeExc, AfterExc) if ( !(x) ) { beforeExc ERROR_ AfterExc }
 #endif
 
+template<typename T>
+const T* find_substr(const T* src, const T* substr);
+
 template<typename T, typename I = ::std::enable_if_t<::std::disjunction_v<::std::is_same<::std::decay_t<T>, char>, ::std::is_same<::std::decay_t<T>, wchar_t>>>>
 class dxString
 {
-    using t_std_string = ::std::conditional_t<::std::is_same_v<T, char>, ::std::string, ::std::wstring>;
 private:
     size_t size;
     T* str;
@@ -58,13 +60,13 @@ public:
     dxString(const dxString<T>& dxstr) noexcept(false) : size(dxstr.size), str(nullptr)
     {
         str = new T[dxstr.size + 1];
-        str = ::std::strcpy(str, dxstr.str);
+        str = ::std::char_traits<T>::copy(str, dxstr.str, ::std::char_traits<T>::length(dxstr.str) + 1);
     }
 
     dxString(dxString<T>&& dxstr) noexcept(false) : size(::std::move(dxstr.size)), str(nullptr)
     {
         str = new T[dxstr.size + 1];
-        str = ::std::strcpy(str, dxstr.str);
+        str = ::std::char_traits<T>::move(str, dxstr.str, ::std::char_traits<T>::length(dxstr.str) + 1);
     }
 
     dxString(T astr) noexcept(false) : size(1ull), str(nullptr)
@@ -74,12 +76,12 @@ public:
         str[1] = 0;
     }
 
-    explicit dxString(const T* astr) noexcept(false) : size(::std::strlen(astr)), str(nullptr)
+    explicit dxString(const T* astr) noexcept(false) : size(::std::char_traits<T>::length(astr)), str(nullptr)
     {
         ER_IF(astr == nullptr, ::std::cerr << "arg is nullptr (constructor(const T*))" << ::std::endl;, )
     
         str = new T[size + 1];
-        str = ::std::strcpy(str, astr);
+        str = ::std::char_traits<T>::copy(str, astr, ::std::char_traits<T>::length(astr) + 1);
     }
 
     ~dxString() noexcept(false)
@@ -97,9 +99,9 @@ public:
         size_t maxszbuf = 4096;                         // temporary size
         T* temp = new T[maxszbuf + 1];
         _stream.getline(temp, ::std::numeric_limits<::std::streamsize>::max(), (T)'\n');
-        instance.size = ::std::strlen(temp);
+        instance.size = ::std::char_traits<T>::length(temp);
         instance.str = new T[instance.size + 1];
-        instance.str = ::std::strcpy(instance.str, temp);
+        instance.str = ::std::char_traits<T>::copy(instance.str, temp, ::std::char_traits<T>::length(temp) + 1);
         delete[] temp;
         return _stream;
     }
@@ -109,7 +111,7 @@ public:
         if (size) delete[] str;
         size = dxstr.size;
         str = new T[dxstr.size + 1];
-        str = ::std::strcpy(str, dxstr.str);
+        str = ::std::char_traits<T>::copy(str, dxstr.str, ::std::char_traits<T>::length(dxstr.str) + 1);
         return *this;
     }
 
@@ -118,7 +120,7 @@ public:
         if (size) delete[] str;
         size = ::std::move(dxstr.size);
         str = new T[dxstr.size + 1];
-        str = ::std::strcpy(str, dxstr.str);
+        str = ::std::char_traits<T>::move(str, dxstr.str, ::std::char_traits<T>::length(dxstr.str) + 1);
         return *this;
     }
 
@@ -126,10 +128,10 @@ public:
     {
         ER_IF(arg == nullptr, ::std::cerr << "arg is nullptr (operator+)!" << ::std::endl;, )
         dxString<T> temp;
-        temp.size = size + ::std::strlen(arg);
+        temp.size = size + ::std::char_traits<T>::length(arg);
         temp.str = new T[temp.size + 1];
-        temp.str = ::std::strcpy(temp.str, str);
-        ::std::strcpy(temp.str + size, arg);
+        temp.str = ::std::char_traits<T>::copy(temp.str, str, ::std::char_traits<T>::length(str) + 1);
+        ::std::char_traits<T>::copy(temp.str + size, arg, ::std::char_traits<T>::length(arg) + 1);
         return temp;
     }
 
@@ -147,8 +149,8 @@ public:
             size_t dsize = size - arg.size;
             temp.size = size + arg.size;
             temp.str = new T[temp.size + 1];
-            temp.str = ::std::strcpy(temp.str, str);
-            ::std::strcpy(temp.str + dsize, arg.str);
+            temp.str = ::std::char_traits<T>::copy(temp.str, str, ::std::char_traits<T>::length(str) + 1);
+            ::std::char_traits<T>::copy(temp.str + dsize, arg.str, ::std::char_traits<T>::length(arg.str) + 1);
         }
         else
         {
@@ -169,7 +171,7 @@ public:
         dxString<T> temp;
         temp.size = n;
         temp.str = new T[temp.size + 1];
-        temp.str = ::std::strncpy(temp.str, str + pos, temp.size);
+        temp.str = ::std::char_traits<T>::copy(temp.str, str + pos, temp.size + 1);
 
         return temp;
     }
@@ -182,7 +184,7 @@ public:
         temp.str = new T[temp.size + 1];
         for(size_t i {0}; i < n; ++i)
         {
-            ::std::strcpy(temp.str + tempsize * i, str);
+            ::std::char_traits<T>::copy(temp.str + tempsize * i, str, ::std::char_traits<T>::length(str) + 1);
         }
 
         return temp;
@@ -220,22 +222,21 @@ public:
 
     dxString<T> operator-(const T* arg) const noexcept(false)
     {
-        size_t arg_size = ::std::strlen(arg);
+        size_t arg_size = ::std::char_traits<T>::length(arg);
         dxString<T> res;
         res.size = size;
         res.str = new T[res.size + 1];
-        const char* cur = str;
-        const char* prev = str;
+        const T* cur = str;
+        const T* prev = str;
         size_t real_size = 0ull;
-        while((cur = ::std::strstr(cur, arg)) != nullptr)
+        while((cur = find_substr(cur, arg)) != nullptr)
         {
-            ::std::strncpy(res.str + real_size, prev, cur - prev);
+            ::std::char_traits<T>::copy(res.str + real_size, prev, cur - prev);
             real_size += cur - prev;
-            res.str[real_size] = 0;
             cur += arg_size;
             prev = cur;
         }
-        ::std::strcpy(res.str + real_size, prev);
+        ::std::char_traits<T>::copy(res.str + real_size, prev, ::std::char_traits<T>::length(prev) + 1);
         return res;
     }
 
@@ -247,21 +248,21 @@ public:
 
     dxString<T> operator-(const dxString<T>& arg) const noexcept(false)
     {
-        size_t arg_size = ::std::strlen(arg.str);
+        size_t arg_size = ::std::char_traits<T>::length(arg.str);
         dxString<T> res;
         res.size = size;
         res.str = new T[res.size + 1];
-        const char* cur = str;
-        const char* prev = str;
+        const T* cur = str;
+        const T* prev = str;
         size_t real_size = 0ull;
-        while((cur = ::std::strstr(cur, arg.str)) != nullptr)
+        while((cur = find_substr(cur, arg.str)) != nullptr)
         {
-            ::std::strncpy(res.str + real_size, prev, cur - prev);
-            real_size = cur - prev;
+            ::std::char_traits<T>::copy(res.str + real_size, prev, cur - prev);
+            real_size += cur - prev;
             cur += arg_size;
             prev = cur;
         }
-        ::std::strcpy(res.str + real_size, prev);
+        ::std::char_traits<T>::copy(res.str + real_size, prev, ::std::char_traits<T>::length(prev) + 1);
         return res;
     }
 
@@ -280,10 +281,10 @@ public:
     {
         ER_IF(arg == nullptr, ::std::cerr << "arg is nullptr (insert)" << ::std::endl;, return false; )
         dxString<T> temp;
-        temp.size = size + ::std::strlen(arg);
+        temp.size = size + ::std::char_traits<T>::length(arg);
         temp.str = new char[temp.size + 1];
-        temp.str = ::std::strcpy(temp.str, str);
-        ::std::strcpy(temp.str + size, arg);
+        temp.str = ::std::char_traits<T>::copy(temp.str, str, ::std::char_traits<T>::length(str) + 1);
+        ::std::char_traits<T>::copy(temp.str + size, arg, ::std::char_traits<T>::length(arg) + 1);
         *this = temp;
         return true;
     }
@@ -292,24 +293,45 @@ public:
 
 int main()
 {
-    dxString<char> main_str;
-    dxString<char> str_to_delete;
-    dxString<char> str_to_replace;
-    ::std::ifstream read("./input.txt");
+    dxString<wchar_t> main_str;
+    dxString<wchar_t> str_to_delete;
+    dxString<wchar_t> str_to_replace;
+    ::std::wifstream read("./input.txt");
     ER_IFN(read.is_open(),, return 1; )
     read >> main_str;
-    ::std::cout << main_str << ::std::endl;
-    dxString temp = ((main_str + "hello!)") - dxString("Hel"));
-    ::std::cout << temp << ::std::endl;
-    temp -= "!";
-    ::std::cout << temp << ::std::endl;
-    temp = temp - (temp - "lo,");
-    ::std::cout << temp << ::std::endl;
+    ::std::wcout << main_str << ::std::endl;
+    dxString temp = ((main_str + L"hello!)") - dxString(L"Hel"));
+    ::std::wcout << temp << ::std::endl;
+    temp -= L"!";
+    ::std::wcout << temp << ::std::endl;
+    temp = temp - (temp - L"lo,");
+    ::std::wcout << temp << ::std::endl;
     read >> str_to_delete;
-    ::std::cout << str_to_delete << ::std::endl;
+    ::std::wcout << str_to_delete << ::std::endl;
     read >> str_to_replace;
-    ::std::cout << str_to_replace * 6 << ::std::endl;
-    ::std::cout << str_to_replace << ::std::endl;
+    ::std::wcout << str_to_replace * 6 << ::std::endl;
+    ::std::wcout << str_to_replace << ::std::endl;
     
     return 0;
+}
+
+template<typename T>
+const T* find_substr(const T* src, const T* substr)
+{
+    size_t match = 0ull;
+    size_t iter = 0ull;
+    size_t arg_size = ::std::char_traits<T>::length(substr);
+    while(*(src + iter))
+    {
+        if (!::std::char_traits<T>::compare(src + iter, substr + match, 1ull))
+        {
+            ++match;
+            ++iter;
+            if (match < arg_size)   continue;
+            return (src + (iter - arg_size));
+        }
+        match = 0ull;
+        ++iter;
+    }
+    return nullptr;
 }
