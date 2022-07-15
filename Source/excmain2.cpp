f85da5e887f4565d238a7fcefa6b3585be8a7097
-dreamIIx <shiba.zheka65@gmail.com>
2022-07-15 22:07:02 +0500

    upd #2 [15.07.2022]

--- Source/excmain2.cpp
+++ Source/excmain2.cpp
@@ -62,23 +62,11 @@ struct Straight
 
 struct Polygon
 {
-//private:
-//    static size_t _id_section;
-//
-//public:
     ::std::deque<Point> dPoints;
-    //::std::vector<size_t> vSections;
 
     Polygon() noexcept(true) {}
 
-    /*static size_t getIdSection() noexcept(true)
-    {
-        return _id_section++;
-    }*/
-
 };
-
-//size_t ::Polygon::_id_section(0ull);
 
 struct DataGraphList
 {
@@ -123,12 +111,10 @@ unsigned _spec_BFS(::std::vector<Polygon> vPolygon, size_t target)
         size_t cur_idx = queue.front();
         unsigned current_range = vDist2Polygon[cur_idx] + 1;
         Polygon cur = vPolygon[cur_idx];
-        //vPolygon.erase(vPolygon.begin() + queue.front());
         queue.pop();
         for(size_t i {0}; i < cur.dPoints.size(); ++i)
         {
             size_t next_i = i < cur.dPoints.size() - 1 ? i + 1 : 0ull;
-            //size_t prev_i = i > 0 ? i - 1 : cur.dPoints.size() - 1;
             if (((cur.dPoints[i].x != cur.dPoints[next_i].x) || ((cur.dPoints[i].x != 0) && (cur.dPoints[i].x != 100)))
                 && ((cur.dPoints[i].y != cur.dPoints[next_i].y) || ((cur.dPoints[i].y != 0) && (cur.dPoints[i].y != 100))))
             {
@@ -139,10 +125,9 @@ unsigned _spec_BFS(::std::vector<Polygon> vPolygon, size_t target)
                         for(size_t k {0}; k < vPolygon[j].dPoints.size(); ++k)
                         {
                             size_t prev_k = k > 0 ? k - 1 : vPolygon[j].dPoints.size() - 1;
-                            //size_t next_k = k < vPolygon[j].dPoints.size() - 1 ? k + 1 : 0ull;
                             if (vPolygon[j].dPoints[k] == cur.dPoints[i])
                             {
-                                if (vPolygon[j].dPoints[prev_k] == cur.dPoints[next_i])/* || (vPolygon[j].dPoints[next_k] == cur.dPoints[prev_i]))*/
+                                if (vPolygon[j].dPoints[prev_k] == cur.dPoints[next_i])
                                 {
                                     if (vDist2Polygon[j] > current_range)
                                     {
@@ -164,77 +149,10 @@ unsigned _spec_BFS(::std::vector<Polygon> vPolygon, size_t target)
                     min_dist_2_target = current_range;
                 }
             }
-            /*if (cur->neighbors[i]->distance > current_range)
-            {
-                cur->neighbors[i]->distance = current_range;
-                queue.push(cur->neighbors[i]);
-            }*/
-            /*if ()
-            {
-                if (min_dist_2_target > ...)
-                {
-                    min_dist_2_target = ...;
-                }
-            }*/
         }
     }
     return min_dist_2_target;
 }
-    
-/*Point intersectionOfStraights(Straight line1, Straight line2) noexcept(false) try
-{
-    return Point((line2.c - line1.c) / (line1.b / line1.a - line2.b / line2.a), (line1.c - line2.c) / (1 - (line2.b * line1.a) / (line2.a * line1.b)) + line1.c);
-}
-catch(...)
-{
-    return Point();
-}
-
-Point intersectionOfStraights(Point f1, Point s1, Point f2, Point s2) noexcept(false) try
-{
-    double det = (f1.x - s1.x) * (f2.y - s2.y) - (f1.y - s1.y) * (f2.x - s2.x);
-    double delt1 = (f1.x * s1.y - f1.y * s1.x);
-    double delt2 = (f2.x * s2.y - f2.y * s2.x);
-    double x = (delt1 * (f2.x - s2.x) - (f1.x * s1.x) * delt2) / det;
-    double y = (delt1 * (f2.y - s2.y) - (f1.y * s1.y) * delt2) / det;
-    return Point(x, y);
-}
-catch(...)
-{
-    return Point();
-}*/
-
-/*struct Section
-{
-    Point a;
-    Point b;
-
-    Section(Point aa, Point ab) : a(aa), b(ab) {}
-
-};*/
-
-/*::std::pair<size_t, Point> nearestStraightToPoint(::std::vector<Straight>& vS, Point& p) noexcept(false)
-{
-    size_t res_idx = 0ull;
-    double distance = ::std::fabs(vS.front().a * p.x + vS.front().b * p.y + vS.front().c) / ::std::sqrt(::std::pow(vS.front().a, 2.) + ::std::pow(vS.front().b, 2.));
-    for(size_t i = 1ull; i < vS.size(); ++i)
-    {
-        double tempDist = ::std::fabs(vS[i].a * p.x + vS[i].b * p.y + vS[i].c) / ::std::sqrt(::std::pow(vS[i].a, 2.) + ::std::pow(vS[i].b, 2.));
-        if (distance > tempDist)
-        {
-            distance = tempDist;
-            res_idx = i;
-        }
-    }
-    auto& ref = vS.at(res_idx);
-    double d = ::std::pow(ref.a, 2.) + ::std::pow(ref.b, 2.);
-    return { res_idx, Point((ref.b * (ref.b * p.x - ref.a * p.y) - ref.a * ref.c) / d, (ref.a * (-ref.b * p.x + ref.a * p.y) - ref.b * ref.c) / d) };
-}*/
-
-/*inline double lengthOfVector(Point& a, Point& b)
-{
-    return ::std::sqrt(::std::pow(b.x - a.x, 2.) + ::std::pow(b.y - a.y, 2.));
-}*/
 
 Point intersectionOfRayAndStraight(Point, Vec2d, const Straight&) noexcept(false);
 Point intersectionOfSectionAndStraight(Point, Point, const Straight&) noexcept(false);
@@ -247,15 +165,11 @@ int main()
     ::std::cin >> n;
     ::std::vector<Polygon> vVertexes;
     vVertexes.reserve(1ull);
-    vVertexes.emplace_back(/*Polygon()*/);
+    vVertexes.emplace_back();
     vVertexes.back().dPoints.push_back(Point(0, 0));
     vVertexes.back().dPoints.push_back(Point(0, 100));
     vVertexes.back().dPoints.push_back(Point(100, 100));
     vVertexes.back().dPoints.push_back(Point(100, 0));
-    /*for(size_t i {0}; i < 4ull; ++i)
-    {
-        vVertexes.back().vSections.push_back(::Polygon::getIdSection());
-    }*/
     for(size_t i {0}; i < n; ++i)
     {
         int x1, x2, y1, y2;
@@ -272,13 +186,9 @@ int main()
     for(size_t i {0}; i < vVertexes.size(); ++i)
     {
         int count = 0;
-        //int min = vVertexes[i].dPoints.front().weight;
-        //int max = vVertexes[i].dPoints.front().weight;
         if (intersectionOfRayAndSection(treasure, Vec2d(1., 0.), vVertexes[i].dPoints.back(), vVertexes[i].dPoints.front())) ++count;
         for(size_t j {0}; j < vVertexes[i].dPoints.size() - 1; ++j)
         {
-            //if (min > vVertexes[i].dPoints[j].weight) min = vVertexes[i].dPoints[j].weight;
-            //else if (max < vVertexes[i].dPoints[j].weight) max = vVertexes[i].dPoints[j].weight;
             if (intersectionOfRayAndSection(treasure, Vec2d(1., 0.), vVertexes[i].dPoints[j], vVertexes[i].dPoints[j + 1])) ++count;
         }
         if (count == 1)
@@ -333,26 +243,17 @@ void splitVertexes(const Straight& line, ::std::vector<Polygon>& vtx)
     {
         Point p1, p2;
         ptrdiff_t _start = -1ll;
-        //int min = vtx[i].dPoints.back().weight;
-        //int max = vtx[i].dPoints.back().weight;
         if ((p1 = intersectionOfSectionAndStraight(vtx[i].dPoints.back(), vtx[i].dPoints[0ull], line)))
         {
-            //::std::cout << "Intersection! [" << vtx[i].dPoints.back().x << ", " << vtx[i].dPoints.back().y << "]-[" << vtx[i].dPoints[0ull].x << ", " << vtx[i].dPoints[0ull].y << ']' << ::std::endl;
-            //p1.weight = vtx[i].dPoints.back().weight;
             _start = 0ull;
-            ////min = (max = p1.weight);
         }
         else
         {
             for(size_t j {0}; j < vtx[i].dPoints.size() - 1; ++j)
             {
-                //if (min > vtx[i].dPoints[j].weight) min = vtx[i].dPoints[j].weight;
-                //else if (max < vtx[i].dPoints[j].weight) max = vtx[i].dPoints[j].weight;
                 if ((p1 = intersectionOfSectionAndStraight(vtx[i].dPoints[j], vtx[i].dPoints[j + 1], line)))
                 {
-                    ////::std::cout << "Intersection! [" << vtx[i].dPoints[j].x << ", " << vtx[i].dPoints[j].y << "]-[" << vtx[i].dPoints[j + 1].x << ", " << vtx[i].dPoints[j + 1].y << ']' << ::std::endl;
                     _start = j + 1;
-                    //p1.weight = vtx[i].dPoints[j].weight;
                     break;
                 }
             }
@@ -360,55 +261,19 @@ void splitVertexes(const Straight& line, ::std::vector<Polygon>& vtx)
         }
         for(size_t j = _start; j < vtx[i].dPoints.size() - 1; ++j)
         {
-            //if (min > vtx[i].dPoints[j].weight) min = vtx[i].dPoints[j].weight;
-            //else if (max < vtx[i].dPoints[j].weight) max = vtx[i].dPoints[j].weight;
             if ((p2 = intersectionOfSectionAndStraight(vtx[i].dPoints[j], vtx[i].dPoints[j + 1], line)))
             {
-                /*for(size_t k = j + 1; k < vtx[i].dPoints.size() - 1; ++k)
-                {
-                    if (min > vtx[i].dPoints[k].weight) min = vtx[i].dPoints[k].weight;
-                    else if (max < vtx[i].dPoints[k].weight) max = vtx[i].dPoints[k].weight;
-                }*/
-                ////::std::cout << "Intersection! [" << vtx[i].dPoints[j].x << ", " << vtx[i].dPoints[j].y << "]-[" << vtx[i].dPoints[j + 1].x << ", " << vtx[i].dPoints[j + 1].y << ']' << ::std::endl;
                 vtx.reserve(vtx.capacity() + 1);
-                vtx.emplace_back(/*Polygon()*/);
+                vtx.emplace_back();
                 vtx.back().dPoints.push_back(p1);
-                //vtx.back().vSections.push_back(::Polygon::getIdSection());
                 for(size_t k = _start; k < j + 1; ++k)
                 {
                     vtx.back().dPoints.push_back(vtx[i].dPoints[k]);
-                    //vtx.back().vSections.push_back(vtx[i].vSections[k]);
                 }
                 vtx.back().dPoints.push_back(p2);
-                //size_t commonSideId = ::Polygon::getIdSection();
-                //vtx.back().vSections.push_back(commonSideId);
-                //p2.weight = vtx[i].dPoints[j].weight;
                 vtx[i].dPoints.erase(vtx[i].dPoints.begin() + _start, j < vtx[i].dPoints.size() - 1 ? vtx[i].dPoints.begin() + j + 1 : vtx[i].dPoints.end());
-                //vtx[i].vSections.erase(vtx[i].vSections.begin() + _start, j < vtx[i].vSections.size() - 1 ? vtx[i].vSections.begin() + j + 1 : vtx[i].vSections.end());
                 vtx[i].dPoints.emplace(vtx[i].dPoints.begin() + _start, p2);
-                //vtx[i].vSections.emplace(vtx[i].vSections.begin() + _start, ::Polygon::getIdSection());
                 vtx[i].dPoints.emplace(vtx[i].dPoints.begin() + _start, p1);
-                //vtx[i].vSections.emplace(vtx[i].vSections.begin() + _start, commonSideId);
-                /*if (min == max)
-                {
-                    vtx.back().dPoints.back().weight = max + 1;
-                    vtx[i].dPoints.at(_start).weight = max + 1;
-                }
-                else
-                {
-                    vtx.back().dPoints.back().weight = max;
-                    vtx[i].dPoints.at(_start).weight = max;
-                }*/
-                /*for(size_t h {0}; h < vtx.back().dPoints.size(); ++h)
-                {
-                    ::std::cout << '[' << vtx.back().dPoints[h].x << ',' << vtx.back().dPoints[h].y << "](" << vtx.back().dPoints[h].weight << ")[";
-                }
-                ::std::cout << ::std::endl;
-                for(size_t h {0}; h < vtx[i].dPoints.size(); ++h)
-                {
-                    ::std::cout << '[' << vtx[i].dPoints[h].x << ',' << vtx[i].dPoints[h].y << "](" << vtx[i].dPoints[h].weight << ")[";
-                }
-                ::std::cout << ::std::endl;*/
                 break;
             }
         }
